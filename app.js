445514245356c75fd25fb0b8ad6e7b632d24cc1d
Christine Magnotta <christinemagnotta@CHRISTINEs-MacBook-Air.local>
2021-01-07 21:47:03 -0600

    adding an image to the page.

--- app.js
+++ app.js
@@ -1,80 +1,24 @@
-d3.json("samples.json").then((data) => {
-	var trace1 = {
-    x: samples.sample_values,
-    y: samples.otu_ids,
-    type: 'bar'
-     }
-};
 
-//Use otu_ids as the labels for the bar chart.
+d3.json("plotly/samples.json").then((importedData) => {
+  console.log(importedData);
+  var belly_data = importedData;
 
+  // Sort the data array using the greekSearchResults value
+  belly_data.sort(function(a, b) {
+    return parseFloat(b.belly_data) - parseFloat(a.belly_data);
+  });
 
-//Use otu_labels as the hovertext for the chart.
+  // Slice the first 10 objects for plotting
+  belly_data = belly_data.slice(0, 10);
 
-
-var data = [trace];
-
-var layout = {
-  title: 'Belly button lint',
-  font:{
-    family: 'Raleway, sans-serif'
-  },
-  showlegend: true,
-  xaxis: {
-    tickangle: -45
-  },
-  yaxis: {
-    zeroline: false,
-    gridwidth: 2
-  },
-  bargap :0.05
-};
-
-
-
-Plotly.newPlot('id="bar"', data, layout);
-
-//bubble chart
 var trace1 = {
-  x: samples.otu_ids,
-  y: samples.sample_values,
-  ode: 'markers',
-  marker: {
-    color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-    opacity: [1, 0.8, 0.6, 0.4],
-    size: [40, 60, 80, 100],
-    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-  }
-};
+    x: data.map(row => row.sample_values),
+    y: data.map(row => row.otu_ids),
+    text: data.map(row => row.otu_ids),
+    name: "Belly Button",
+    type: "bar",
+    orientation: "h"
+  };
 
-var data1 = [trace1];
-
-var layout1 = {
-  title: 'Belly Button Lint Bubble Chart',
-  showlegend: true,
-  height: 600,
-  width: 600,
-};
-
-
-//Use sample_values for the marker size.
-
-
-//Use otu_ids for the marker colors.
-
-
-//Use otu_labels for the text values.
-
-//Display the sample metadata, i.e., an individual's demographic information.
-Plotly.newPlot('id="bubble"', data1, layout1);
-
-//Display each key-value pair from the metadata JSON object somewhere on the page.
-var sample-metadata = samples.metadata;
-
-//check to make sure this is correct.
-
-//Update all of the plots any time that a new sample is selected.
-
-
-
-
+ var bardata = [trace1];
+ Plotly.newPlot("plot", bardata, layout);
