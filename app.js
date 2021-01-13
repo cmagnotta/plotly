75d67a1181b70ab93db9f2a50c871f33088a2660
Christine Magnotta <christinemagnotta@CHRISTINEs-MacBook-Air.local>
2021-01-13 00:45:21 -0600

    Added the gauge, made some fixes.

--- app.js
+++ app.js
@@ -25,15 +25,16 @@ function getData() {
   var traceBub = {
     x: otu_ids,
     y: sample,
-    text: sample,
-    name: "Belly Button Bubble Chart",
+    text: otu_labels,
     mode: 'markers',
     marker: {
-    size: [40, 60, 80, 100]
+      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+      opacity: [1, 0.8, 0.6, 0.4],
+      size: [40, 60, 80, 100]
     }
   } 
   var layoutBub = {
-    title: 'Marker Size',
+    title: 'Bacteria Frequency',
       showlegend: true,
       height: 600,
       width: 600
@@ -48,13 +49,43 @@ function getData() {
     }
 
     var layoutBar = {
-      title: "This is my bar chart",
+      title: "Most Common Bacteria",
     }
 
     var dataBar = [traceBar]
     var dataBub = [traceBub];
     Plotly.newPlot("bubble", dataBub, layoutBub);
     Plotly.newPlot("bar", dataBar, layoutBar);
+
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: metadata.wfreq,
+        title: { text: "Wash Frequency" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        gauge: {
+          axis: { range: [null, 10] },
+          steps: [
+            { range: [0,1], color: "#ff0000" },
+            { range: [1,2], color: "#ff9999" },
+            { range: [2,3], color: "#ffe6e6" },
+            { range: [3,4], color: "#ffe6e6" },
+            { range: [4,5], color: "#FFF0F5" },
+            { range: [5,6], color: "#F0FFFF" },
+            { range: [6,7], color: "#ADD8E6" },
+            { range: [7,8], color: "#87CEEB" },
+            { range: [8,9], color: "#1E90FF" },
+            { range: [9,10], color: "#0000FF" }
+          ],
+          
+          }
+        }
+      
+    ];
+    
+    var gaugeLayout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
 
     d3.select("#metadata").html("")
     d3.select("#metadata").append("p").text("ID: " + metadata.id);
